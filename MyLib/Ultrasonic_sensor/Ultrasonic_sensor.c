9e025c253f6eb1a7ee03fd4e9c188f73700d0d1c
ahmadmorad39 <ahmadmorad39@gmail.com>
2024-10-25 15:36:43 +0300

    Added comments

--- MyLib/Ultrasonic_sensor/Ultrasonic_sensor.c
+++ MyLib/Ultrasonic_sensor/Ultrasonic_sensor.c
@@ -19,11 +19,11 @@ uint32_t Measure_distance(void)
     int lastEdge, thisEdge;
 
       /* Given 10us trigger pulse */
-      GPIO_PORTA_DATA_R &= ~(1<<4); /* make trigger  pin high */
+      GPIO_PORTA_DATA_R &= ~(1<<4); /* make trigger to PA4 pin low */
       Delay_MicroSecond(10); /*10 seconds delay */
-      GPIO_PORTA_DATA_R |= (1<<4); /* make trigger  pin high */
+      GPIO_PORTA_DATA_R |= (1<<4); /* make trigger to PA4 pin high */
       Delay_MicroSecond(10); /*10 seconds delay */
-      GPIO_PORTA_DATA_R &= ~(1<<4); /* make trigger  pin low */
+      GPIO_PORTA_DATA_R &= ~(1<<4); /* make trigger to PA4 pin low */
 
     while(1)
     {
@@ -48,16 +48,17 @@ void Timer0ACapture_init(void)
     SYSCTL_RCGCTIMER_R |= 1;     /* enable clock to Timer Block 0 */
     SYSCTL_RCGCGPIO_R |= 2;      /* enable clock to PORTB */
 
+    // Initiate PB6
     GPIO_PORTB_DIR_R &= ~0x40;        /* make PB6 an input pin */
     GPIO_PORTB_DEN_R |= 0x40;         /* make PB6 as digital pin */
     GPIO_PORTB_AFSEL_R |= 0x40;       /* use PB6 alternate function */
     GPIO_PORTB_PCTL_R &= ~0x0F000000;  /* configure PB6 for T0CCP0 */
     GPIO_PORTB_PCTL_R |= 0x07000000;
 
-      /* PB2 as a digital output signal to provide trigger signal */
-      SYSCTL_RCGCGPIO_R |= 1;      /* enable clock to PORTA */
-      GPIO_PORTA_DIR_R |=(1<<4);         /* set PB2 as a digial output pin */
-      GPIO_PORTA_DEN_R |=(1<<4);         /* make PB2 as digital pin */
+    /* PA4 as a digital output signal to provide trigger signal */
+    SYSCTL_RCGCGPIO_R |= 1;      /* enable clock to PORTA */
+    GPIO_PORTA_DIR_R |=(1<<4);         /* set PA4 as a digial output pin */
+    GPIO_PORTA_DEN_R |=(1<<4);         /* make PA4 as digital pin */
 
     TIMER0_CTL_R &= ~1;          /* disable timer0A during setup */
     TIMER0_CFG_R = 4;            /* 16-bit timer mode */
@@ -77,7 +78,7 @@ void Delay_MicroSecond(int time)
     TIMER1_CTL_R = 0;            /* disable Timer before initialization */
     TIMER1_CFG_R = 0x04;         /* 16-bit option */
     TIMER1_TAMR_R = 0x02;        /* periodic mode and down-counter */
-    TIMER1_TAILR_R = 16 - 1;  /* TimerA interval load value reg */
+    TIMER1_TAILR_R = 16 - 1;  /* TimerA interval load value reg .The choice of 16 here is based on the assumption that the system clock frequency is 16 MHz*/
     TIMER1_ICR_R = 0x1;          /* clear the TimerA timeout flag */
     TIMER1_CTL_R |= 0x01;        /* enable Timer A after initialization */
 
@@ -85,37 +86,6 @@ void Delay_MicroSecond(int time)
     {
         while ((TIMER1_RIS_R & 0x1) == 0) ;      /* wait for TimerA timeout flag */
         TIMER1_ICR_R = 0x1;      /* clear the TimerA timeout flag */
-    }
-}
-void UART5_init(void)
-{
-      SYSCTL_RCGCUART_R |= 0x20;  /* enable clock to UART5 */
-    SYSCTL_RCGCGPIO_R |= 0x10;  /* enable clock to PORTE for PE4/Rx and RE5/Tx */
-    /* UART0 initialization */
-    UART5_CTL_R = 0;         /* UART5 module disbable */
-    UART5_IBRD_R = 104;      /* for 9600 baud rate, integer = 104 */
-    UART5_FBRD_R = 11;       /* for 9600 baud rate, fractional = 11*/
-    UART5_CC_R = 0;          /*select system clock*/
-    UART5_LCRH_R = 0x60;     /* data lenght 8-bit, not parity bit, no FIFO */
-    UART5_CTL_R = 0x301;     /* Enable UART5 module, Rx and Tx */
-
-    /* UART5 TX5 and RX5 use PE4 and PE5. Configure them digital and enable alternate function */
-    GPIO_PORTE_DEN_R = 0x30;      /* set PE4 and PE5 as digital */
-    GPIO_PORTE_AFSEL_R = 0x30;    /* Use PE4,PE5 alternate function */
-    GPIO_PORTE_AMSEL_R = 0;    /* Turn off analg function*/
-    GPIO_PORTE_PCTL_R = 0x00110000;     /* configure PE4 and PE5 for UART */
-}
-void UART5_Transmitter(unsigned char data)
-{
-    while((UART5_FR_R & (1<<5)) != 0); /* wait until Tx buffer not full */
-    UART5_DR_R = data;                  /* before giving it another byte */
-}
-
-void printstring(char *str)
-{
-  while(*str)
-    {
-        UART5_Transmitter(*(str++));
     }
 }
 void Delay(unsigned long counter)
